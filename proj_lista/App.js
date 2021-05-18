60034d1b09a5e5457f98acf1a4f705f5e76f6cea
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-05-18 18:45:08 -0300

    Create App.js

---
+++ proj_lista/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Feed from './components/Feed';
+
+class App extends Component{
+
+
+    constructor(props){
+        super(props);
+        this.state = {
+            feed:[
+                {id: 1, username: 'Matheus', curtidas:10, comentarios:2},
+                {id: 2, username: 'Lucas', curtidas:120, comentarios:24},
+                {id: 1, username: 'Amanda', curtidas:30, comentarios:12},
+            ]
+        };
+
+    }
+
+
+    render(){
+        return(
+            <Feed id={item.id} username={item.username}
+                curtidas={item.curtidas} comentarios={item.comentarios}/>
+        );
+    }
+}
+
+export default App;
